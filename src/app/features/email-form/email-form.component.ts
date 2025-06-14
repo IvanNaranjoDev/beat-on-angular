c6d8a8d0c8743685480279f95994c71f87dc38ac
IvanNaranjoEducaand <inarmar@g.educaand.com>
2025-06-14 18:33:00 +0200

    Cambio Email para reconocer i

--- src/app/features/email-form/email-form.component.ts
+++ src/app/features/email-form/email-form.component.ts
@@ -36,6 +36,14 @@ export class EmailFormComponent implements OnInit {
 
   enviarEmail() {
     this.emailService.postMail(this.email).subscribe({
+      next: (response) => {
+        console.log('Correo enviado correctamente', response);
+        alert('Correo enviado con éxito');
+      },
+      error: (error) => {
+        console.error('Error al enviar el correo', error);
+        alert('Error al enviar el correo. Verifica los datos o contacta al administrador.');
+      }
     });
   }
 }
